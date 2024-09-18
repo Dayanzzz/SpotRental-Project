0d9a86dcedbb0ecccd6f3d8e9437ba571ea7ecfd
Dayanzzz <zakrzewski.dayan@gmail.com>
2024-09-18 16:30:42 -0700

    setToken-cookie-working

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,25 +1,33 @@
 // backend/routes/api/index.js
+
 const router = require('express').Router();
+
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+
+
+
+
+router.get('/set-token-cookie', async (_req, res) => {
+    const user = await User.findOne({
+      where: {
+        username: 'Demo-lition'
+      }
+    });
+    setTokenCookie(res, user);
+    return res.json({ user: user });
+  });
+  
+
+
+
+
 
 
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
   });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ 
 
 module.exports = router;
