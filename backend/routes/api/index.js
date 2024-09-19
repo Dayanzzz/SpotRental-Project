a81699a65a49a8bcdec628bc3d465f41595809ed
Dayanzzz <zakrzewski.dayan@gmail.com>
2024-09-19 15:20:13 -0700

    commit merge

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,21 +1,11 @@
 // backend/routes/api/index.js
 
 const router = require('express').Router();
-const sessionRouter = require('./session.js');
-const usersRouter = require('./users.js');
-const { restoreUser } = require('../../utils/auth.js')
-const {  setTokenCookie } = require('../../utils/auth.js');
+const { restoreUser, setTokenCookie, requireAuth } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
 
 router.use(restoreUser);
 
-router.use('/session', sessionRouter);
-
-router.use('/users', usersRouter);
-
-
-
-const { requireAuth } = require('../../utils/auth.js');
 
 router.get('/set-token-cookie', async (_req, res) => {
     const user = await User.findOne({
