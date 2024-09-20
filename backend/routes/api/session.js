81b6eac8de1d72119eaf43cf1469b27c93279b01
AbramBuck <abram.buck@gmail.com>
2024-09-20 17:03:23 -0500

    "Save changes to sessions.js, users.js, and auth.js. These are related to the name refactor'

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -52,6 +52,8 @@ router.post(
       id: user.id,
       email: user.email,
       username: user.username,
+      firstName: user.firstName,
+      lastName: user.lastName
     };
 
     await setTokenCookie(res, safeUser);
@@ -88,6 +90,8 @@ router.post(
         id: user.id,
         email: user.email,
         username: user.username,
+        firstName: user.firstName,
+        lastName: user.lastName
       };
   
       await setTokenCookie(res, safeUser);
@@ -116,6 +120,8 @@ router.post(
           id: user.id,
           email: user.email,
           username: user.username,
+          firstName: user.firstName,
+          lastName: user.lastName
         };
         return res.json({
           user: safeUser
