59ad365ed32227399840fad01eeaf6745478f32e
AbramBuck <abram.buck@gmail.com>
2024-09-20 23:08:53 -0500

    updated seeder file

--- backend/db/seeders/20240918215018-demo-user.js
+++ backend/db/seeders/20240918215018-demo-user.js
@@ -1,9 +1,8 @@
 'use strict';
-const { Op } = require('sequelize');
 const { User } = require('../models');
 const bcrypt = require("bcryptjs");
 
-let options = { tableName: 'Users' };
+let options = {};
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
 }
@@ -36,6 +35,8 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
+    options.tableName = 'Users';
+    const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
       username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
     }, {});
